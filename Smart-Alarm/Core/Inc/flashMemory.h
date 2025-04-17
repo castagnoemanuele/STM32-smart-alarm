01624eecea59d1c881dff4598f4bd91d157487d6
castagnoemanuele <castagnoemanuele3@gmail.com>
2025-04-17 16:53:37 +0200

    minor fixes

--- Smart-Alarm/Core/Inc/flashMemory.h
+++ Smart-Alarm/Core/Inc/flashMemory.h
@@ -2,8 +2,8 @@
 #define FLASH_MEMORY_H
 #include <stdint.h>
 
-#define Address1 0x08040000
-#define Address2 0x08040040
+#define address1 0x08040000 //Address to save the password of user 1
+#define address2 0x08040040 //Address to save the password of user 2
 
 /**
  * @brief Saves a 32-bit unsigned integer to flash memory.
@@ -16,7 +16,7 @@
  *
  * @param data The 32-bit unsigned integer to be stored in flash memory.
  */
-void SaveToFlash(uint32_t data);
+void SaveToFlash(uint32_t data, uint32_t address);
 
 
 /**
@@ -26,5 +26,5 @@ void SaveToFlash(uint32_t data);
  *
  * @return The 32-bit unsigned integer retrieved from flash memory.
  */
-uint32_t ReadFromFlash(void);
+uint32_t ReadFromFlash(uint32_t address);
 #endif // FLASH_MEMORY_H
--- Smart-Alarm/Core/Src/flashMemory.c
+++ Smart-Alarm/Core/Src/flashMemory.c
@@ -2,24 +2,32 @@
 #include <stdint.h>           // <-- Don't forget him here too!
 #include "stm32f4xx_hal.h"    // Required for HAL_FLASH functions
 
-void SaveToFlash(uint32_t data) {
-    uint32_t address = Address1;
 
-
+/**
+ * @brief Saves a 32-bit word to the specified Flash memory address.
+ * @param data The 32-bit data to be written to Flash.
+ * @param address The Flash memory address where the data will be stored.
+ */
+void SaveToFlash(uint32_t data, uint32_t address) {
+    // Unlock the Flash memory for write access
     HAL_FLASH_Unlock();
 
     FLASH_EraseInitTypeDef eraseInit;
     uint32_t SectorError = 0;
 
+    // Configure the erase parameters for one sector
     eraseInit.TypeErase = FLASH_TYPEERASE_SECTORS;
     eraseInit.VoltageRange = FLASH_VOLTAGE_RANGE_3;
     eraseInit.Sector = FLASH_SECTOR_6;
     eraseInit.NbSectors = 1;
 
+    // Erase the specified Flash sector
     HAL_FLASHEx_Erase(&eraseInit, &SectorError);
 
+    // Program the Flash memory with the specified data at the given address
     HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, address, data);
 
+    // Lock the Flash memory to prevent further writes
     HAL_FLASH_Lock();
 }
 
@@ -27,6 +35,6 @@ void SaveToFlash(uint32_t data) {
  * @brief Reads a 32-bit unsigned integer from flash memory.
  * @return The 32-bit unsigned integer retrieved from flash memory.
  */
-uint32_t ReadFromFlash(void) {
+uint32_t ReadFromFlash(uint32_t address) {
 	 return *(uint32_t*) 0x08040000;
 }
